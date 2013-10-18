779e155fd5a3de4ab9d0552f9f09cbbab24f9f5f
tcoy <trevor8_@hotmail.com>
2013-10-17 21:01:20 -0400

    Camera/Tiling

--- MOBA/MOBA/Main.cs
+++ MOBA/MOBA/Main.cs
@@ -8,83 +8,79 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using MOBA.Assets;
 
 namespace MOBA
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
     public class Main : Microsoft.Xna.Framework.Game
     {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        public GraphicsDeviceManager graphics;
+        public SpriteBatch spriteBatch;
+
+        public AssetManager assets;
+
+        Map map;
+
+        public int WIDTH, HEIGHT;
 
         public Main()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferWidth = 1024;
+            graphics.PreferredBackBufferHeight = 768;
+
+            WIDTH = graphics.PreferredBackBufferWidth;
+            HEIGHT = graphics.PreferredBackBufferHeight;
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            assets = new AssetManager();
+            map = new Map(this);
 
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            assets.storeTexture(Content.Load<Texture2D>("Enviroment/Grass"), new Rectangle(0, 0, 64, 64)); // ID 0
+            assets.storeTexture(Content.Load<Texture2D>("Enviroment/Tree"), new Rectangle(0, 0, 96, 192)); // ID 1
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            // Allows the game to exit
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
 
-            // TODO: Add your update logic here
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+                map.cam.Translate(0, 5);
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+                map.cam.Translate(0, -5);
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+                map.cam.Translate(5, 0);
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+                map.cam.Translate(-5, 0);
 
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
+            spriteBatch.Begin();
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            map.Draw();
 
+            spriteBatch.End();
             base.Draw(gameTime);
         }
     }
---
+++ MOBA/MOBA/World/Map.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.Diagnostics;
+using System.IO;
+
+namespace MOBA
+{
+    public class Camera
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        bool paused = false;
+
+        public Camera()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+        public void Translate(int x, int y)
+        {
+            if (!paused)
+            {
+                X += x;
+                Y += y;
+            }
+        }
+
+        public void Pause()
+        {
+            paused = true;
+        }
+
+        public void unPause()
+        {
+            paused = false;
+        }
+    }
+    public class Map
+    {
+        const int width = 2048;
+        const int height = 1280;
+
+        public Camera cam;
+
+        Main m;
+
+        public Map(Main main)
+        {
+            m = main;
+
+            cam = new Camera();
+        }
+
+        public void Draw()
+        {
+            for (int x = 0 - width / 2; x < width / 2; x += 64)
+            {
+                for (int y = 0; y < height; y += 64)
+                {
+                    m.spriteBatch.Draw(m.assets.getTexture(0).texture, new Rectangle(x + cam.X, y + cam.Y, 64, 64), Color.White);
+                }
+            }
+        }
+    }
+}
