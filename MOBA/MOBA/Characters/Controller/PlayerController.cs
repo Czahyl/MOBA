e60e1780af6bc63e8d475bcec88a8aae8779d290
Czahyl <czahyl@gmail.com>
2013-10-20 18:09:31 -0400

    Input classes, logic cleaning, entity base changes

--- MOBA/MOBA/Characters/Controller/PlayerController.cs
+++ MOBA/MOBA/Characters/Controller/PlayerController.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 using MOBA.Characters.Prototype;
+using MOBA.Input;
 
 namespace MOBA.Characters.Controller
 {
+    /*Handles input and 'controls' the local player*/
     public class PlayerController : Controller
     {
+        private InputHandler input = new InputHandler();
+
         public PlayerController(Main main) : base(main)
         {
-            
+            //Kinds of messy, but gets the job done
+            input.handleKeypress += delegate(Keys key)
+            {
+                
+            };
+
+            input.handleKeyup += delegate(Keys k)
+            {
+
+            };
+
+            input.handleClick += delegate(MouseButton button, int x, int y)
+            {
+
+            };
         }
 
         public Player getPlayer()
---
+++ MOBA/MOBA/Input/InputHandler.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace MOBA.Input
+{
+    public delegate void keyPressHandle(Keys k);
+    public delegate void keyUpHandle(Keys k);
+    public delegate void clickHandle(MouseButton button, int x, int y);
+
+    public class InputHandler
+    {
+        private KeyboardState oldstate = Keyboard.GetState();
+        private MouseState oldmouse = Mouse.GetState();
+
+        public event keyPressHandle handleKeypress;
+        public event keyUpHandle handleKeyup;
+        public event clickHandle handleClick;
+
+        public void Listen()
+        {
+            KeyboardState keystate = Keyboard.GetState();
+            MouseState mouse = Mouse.GetState();
+
+            foreach (Keys key in Enum.GetValues(typeof(Keys)))
+            {
+                if (keystate.IsKeyDown(key) && oldstate.IsKeyUp(key))
+                {
+                    onKeypress(key);
+                }
+
+                if (keystate.IsKeyUp(key) && oldstate.IsKeyDown(key))
+                {
+                    onKeyup(key);
+                }
+            }
+
+            if (mouse.LeftButton == ButtonState.Pressed && oldmouse.LeftButton == ButtonState.Released)
+            {
+                onClick(MouseButton.Left, mouse.X, mouse.Y);
+            }
+
+            if (mouse.RightButton == ButtonState.Pressed && oldmouse.RightButton == ButtonState.Released)
+            {
+                onClick(MouseButton.Right, mouse.X, mouse.Y);
+            }
+
+            oldstate = keystate;
+            oldmouse = mouse;
+        }
+
+        protected virtual void onKeyup(Keys key)
+        {
+
+        }
+
+        protected virtual void onKeypress(Keys key)
+        {
+
+        }
+
+        protected virtual void onClick(MouseButton button, int x, int y)
+        {
+            
+        }
+    }
+
+    public enum MouseButton { Left, Right }
+}
