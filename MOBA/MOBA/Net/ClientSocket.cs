4086a7cfa920399588e6a06968b79721ff2b7d03
Czahyl <czahyl@gmail.com>
2013-10-21 12:20:20 -0400

    Beginnings of networking

---
+++ MOBA/MOBA/Net/ClientSocket.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace MOBA.Net
+{
+    public class ClientSocket
+    {
+        private AddressFamily addrFamily;
+        private ProtocolType protocol;
+        private Socket sock;
+        private PacketWriter writer;
+        private PacketReader reader;
+
+        public ClientSocket(AddressFamily family = AddressFamily.InterNetworkV6, ProtocolType protocol = ProtocolType.IPv6)
+        {
+            addrFamily = family;
+            this.protocol = protocol;
+        }
+
+        public void Connect(string ip, int port)
+        {
+            sock = new Socket(addrFamily, SocketType.Stream, protocol);
+            sock.Connect(new IPEndPoint(IPAddress.Parse(ip), port));
+        }
+
+        public void Send(Packet packet)
+        {
+            writer = new PacketWriter();
+            packet.Write(writer);
+            sock.Send(writer.Buffer());
+        }
+
+        public void Read(Packet packet)
+        {
+            reader = new PacketReader(new MemoryStream(packet.Buffer()));
+            packet.Process(reader);
+        }
+    }
+}
---
+++ MOBA/MOBA/Net/Packet.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace MOBA.Net
+{
+    public class Headers
+    {
+        public static short HANDSHAKE = 0;
+    }
+
+    public class PacketWriter : BinaryWriter
+    {
+        private MemoryStream ms;
+
+        public PacketWriter() : base(new MemoryStream())
+        {
+            ms = (MemoryStream)OutStream;
+        }
+
+        public byte[] Buffer()
+        {
+            return ms.ToArray();
+        }
+    }
+
+    public class PacketReader : BinaryReader
+    {
+        public PacketReader(MemoryStream stream) : base(stream)
+        {
+
+        }
+    }
+
+    public abstract class Packet
+    {
+        public short Header
+        { get; protected set; }
+
+        public abstract void Process(PacketReader reader);
+        public abstract void Write(PacketWriter writer);
+        public abstract byte[] Buffer();
+        public abstract void setBuffer(byte[] dat);
+    }
+
+    public class HandshakePacket : Packet
+    {
+        private byte[] dat;
+
+        public HandshakePacket()
+        {
+            Header = Headers.HANDSHAKE;
+        }
+
+        public override void Process(PacketReader reader)
+        {
+            bool ok = reader.ReadBoolean();
+        }
+
+        public override void Write(PacketWriter writer)
+        {
+            writer.Write(Header);
+        }
+
+        public override byte[] Buffer()
+        {
+            return dat;
+        }
+
+        public override void setBuffer(byte[] dat)
+        {
+            this.dat = dat;
+        }
+    }
+}
