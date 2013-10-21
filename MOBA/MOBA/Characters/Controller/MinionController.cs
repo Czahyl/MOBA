25957bd012221a16f1fb3e38fb51f44d4253b5ab
tcoy <trevor8_@hotmail.com>
2013-10-21 14:03:36 -0400

    Minion Startings

---
+++ MOBA/MOBA/Characters/Controller/MinionController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MOBA.Characters.Prototype;
+using MOBA.World;
+
+namespace MOBA.Characters.Controller
+{
+    public class MinionController
+    {
+        private Main m;
+        protected Minion minion;
+
+        public MinionController(Main main)
+        {
+            m = main;
+        }
+
+        public Minion getEntity()
+        {
+            return minion;
+        }
+
+        public void plugEntity(Minion entity)
+        {
+            minion = entity;
+
+            minion.light = new LightEmitter(m.lightEngine, (int)minion.Center.X, (int)minion.Center.Y, 50, false);
+
+            m.lightEngine.plugEmitter(minion.light);
+        }
+
+        public void Update()
+        {
+            minion.Update();
+        }
+
+        public void Draw()
+        {
+            minion.Draw(m.spriteBatch);
+        }
+    }
+}
---
+++ MOBA/MOBA/Characters/Prototype/Minion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MOBA.World;
+
+namespace MOBA.Characters.Prototype
+{
+    public class Minion : BaseEntity
+    {
+        public const int WIDTH = 32;
+        public const int HEIGHT = 32;
+        public const int WORTH = 10;
+
+        public Minion()
+        {
+            //Add preset animation to ani buffer
+            //Maxhealth = Base amount * Towers dead
+            Center = new Vector2(150, 150);
+            Bounds = new Rectangle((int)Center.X, (int)Center.Y, WIDTH, HEIGHT);
+            light = new LightEmitter();
+        }
+
+        public override void Update()
+        {
+            Center = new Vector2(150 + Camera.X, 150 + Camera.Y);
+            base.Update();
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
+    }
+}
